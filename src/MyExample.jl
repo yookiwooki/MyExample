0a8b178107b6d208a1a8298fb787f7c0ea10c96d
Sean McArdle <seanmcardle14@gmail.com>
2020-06-02 18:11:57 -0500

    working on tests

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,7 @@
 module MyExample
 
-# Write your package code here.
+include("extra_file.jl")
+
+export my_f
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using MyExample
 using Test
 
-@testset "MyExample.jl" begin
-    # Write your tests here.
+@testset "preliminary test" begin
+    @test my_f(2,1)==5
 end
